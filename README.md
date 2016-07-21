0784ab278df105e0061d90339062e2f47a62714c
Jane Fong <janeefongg@gmail.com>
2016-07-20 17:10:57 -0700

    [setup] add work list in README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # react-todo-app
+
+##To do list:
+- Persistent storage
+- Set up redux
+- Draw application structure
+- Create endpoints
--- src/index.js
+++ src/index.js
@@ -4,14 +4,3 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 
 ReactDOM.render(<App />, document.querySelector('.container'));
-//
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-//
-// const createStoreWithMiddleware = applyMiddleware()(createStore);
-//
-// ReactDOM.render(
-//   <Provider store={createStoreWithMiddleware(reducers)}>
-//     <App />
-//   </Provider>
-//   , document.querySelector('.container'));
