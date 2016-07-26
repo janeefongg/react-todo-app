802514ccc88e10801c29997f569b5cf58cbcc5e3
Jane Fong <janeefongg@gmail.com>
2016-07-25 18:50:43 -0700

    [setup] mongoose

--- db.js
+++ db.js
@@ -1,7 +1,30 @@
-var MongoClient = require('mongodb').MongoClient;
+// var MongoClient = require('mongodb').MongoClient;
+//
+// MongoClient.connect("mongodb://localhost:27017/TasksDb", function(err, db) {
+//   if(err) { return console.dir(err); }
+//   console.log('database connection established!');
+//
+// });
 
-MongoClient.connect("mongodb://localhost:27017/TasksDb", function(err, db) {
-  if(err) { return console.dir(err); }
-  console.log('database connection established!');
+var mongoose = require('mongoose');
+var timestamp = require('mongoose-times');
 
+var dbURL = 'mongodb://localhost:27017/TasksDb';
+
+var TaskSchema = mongoose.Schema({
+  title: String,
+  body: String
 });
+
+TaskSchema.plugin(timestamp);
+
+mongoose.model('Tasks', TaskSchema);
+
+mongoose.connect(dbURL);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Database connection established.');
+})
