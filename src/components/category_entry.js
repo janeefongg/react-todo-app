74a8ea8d24aab6daff58b222535490821bf32813
Jane Fong <janeefongg@gmail.com>
2016-08-18 16:15:00 -0700

    [refactor] category form

--- src/components/category_entry.js
+++ src/components/category_entry.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CategoryEntry = ({item, select}) => {
 
   return (
-    <div onClick={() => select(item)}>
+    <div onClick={() => select(item)} className="category-entry">
       {item}
     </div>
   )
--- src/components/category_list.js
+++ src/components/category_list.js
@@ -1,18 +1,115 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Modal from 'react-modal';
 
+import CategoryForm from './category_form';
 import CategoryEntry from './category_entry';
 
-const CategoryList = ({categories, selectCategory}) => {
-  const categoriesArr = categories.map((category, index) => {
-    return <CategoryEntry key={index} select={selectCategory} item={category} />
-  });
+//TODO: update state to have categories and pass down as props
 
-  return (
-    <div>
-      {categoriesArr}
-    </div>
-  )
+export default class CategoryList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalIsOpen: false
+    }
+
+    this.openModal = this.openModal.bind(this);
+    // this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  // afterOpenModal() {
+  //   this.refs.subtitle.style.color = "000";
+  // }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  render() {
+    console.log('this.props.categories', this.props.categories)
+    const categoriesArr = this.props.categories.map((category, index) => {
+      return <CategoryEntry key={index} select={this.props.selectCategory} item={category}/>
+    });
+
+    return (
+      <div>
+        <div>
+          {categoriesArr}
+        </div>
+        <div>
+          <button onClick={this.openModal}>Create Category</button>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+          >
+            <button onClick={this.closeModal}>close</button>
+            <CategoryForm closeModal={this.closeModal}/>
+          </Modal>
+        </div>
+      </div>
+    )
+  }
 }
 
 
-export default CategoryList;
+// export default class CategoryList extends Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       modalIsOpen: false
+//     }
+//
+//     this.openModal = this.openModal.bind(this);
+//     this.afterOpenModal = this.afterOpenModal.bind(this);
+//     this.closeModal = this.closeModal.bind(this);
+//   }
+//
+//   openModal() {
+//     this.setState({modalIsOpen: true});
+//   }
+//
+//   afterOpenModal() {
+//     this.refs.subtitle.style.color = "000";
+//   }
+//
+//   closeModal() {
+//     this.setState({modalIsOpen: false});
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <h3>Task Keeper</h3>
+//         {/*<div>*/}
+//         {/*<button onClick={this.openModal}>Create Category</button>*/}
+//         {/*<Modal*/}
+//         {/*isOpen={this.state.modalIsOpen}*/}
+//         {/*onAfterOpen={this.afterOpenModal}*/}
+//         {/*onRequestClose={this.closeModal}*/}
+//         {/*>*/}
+//         {/*<button onClick={this.closeModal}>close</button>*/}
+//         {/*<CategoryForm closeModal={this.closeModal} />*/}
+//         {/*</Modal>*/}
+//         {/*</div>*/}
+//         <div>
+//           {this.props.children}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+//
+//
+//
+//
+//
+//
+// export default CategoryList;
